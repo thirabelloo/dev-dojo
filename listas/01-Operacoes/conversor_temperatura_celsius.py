6bf3dd60cb7fe032139d3b7e8e48229d2e1aa877
Thiago Rabello <thiagorabello72@gmail.com>
2024-11-29 18:41:50 -0300

    4 - Escreva um programa que realize a conversão de graus Fahrenheit (F)  para graus Celsius (C).

---
+++ listas/01-Operacoes/conversor_temperatura_celsius.py
@@ -0,0 +1,17 @@
+"""Script que realiza a conversão de graus Fahrenheit (F) para graus Celsius (C)."""
+
+
+def converter_fahrenheit_para_celsius(graus_fahrenheit):
+    """
+    Converte uma temperatura de Fahrenheit para Celsius.
+
+    Parâmetros:
+    graus_fahrenheit (float): A temperatura em graus Fahrenheit.
+
+    Retorna:
+    str: A temperatura convertida em Celsius, formatada com duas casas decimais.
+    """
+    if not isinstance(graus_fahrenheit, (int, float)):
+        raise TypeError("A temperatura deve ser um numero")
+    celsius = ((graus_fahrenheit - 32) * 5) / 9
+    return f"A temperatura em Fahrenheit é: {celsius:.2f}(C)"
