4b867d46a48b9100f9436c477d11439bd6880be4
Thiago Rabello <thiagorabello72@gmail.com>
2024-12-04 15:50:23 -0300

    17 - Sabendo que 1 ft é igual a 0,3048 m ou 30,48 cm, desenvolva um programa que leia uma medida em pés e calcule o equivalente em metros.

---
+++ listas/01-Operacoes/conversor_unidade.py
@@ -0,0 +1,27 @@
+"""Script que ler uma medida em pés e calcule o equivalente em metros.
+"""
+
+
+def conversor_unidade_ft():
+    """
+    Converte uma medida em pés (ft) para metros e centímetros.
+
+    Solicita ao usuário que digite uma medida em pés, converte a medida para metros e centímetros, e exibe os resultados.
+
+    Retorna: None: Esta função não retorna nenhum valor. Os resultados são impressos diretamente.
+    """
+
+    try:
+        numero_ft = float(input("Digite o valor da medida em pes(ft):"))
+    except ValueError:
+        print("Entrada inválida. Por favor, a medida deve ser um numero.")
+        return
+
+    ft_para_centimetros = numero_ft * 0.3048
+    ft_para_metros = numero_ft * 30.48
+
+    print(
+        f"Convertendo {numero_ft} ft:\n"
+        f"{ft_para_metros:.2f} metros\n"
+        f"{ft_para_centimetros:.2f} centímetros"
+    )
